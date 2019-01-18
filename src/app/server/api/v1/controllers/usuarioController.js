a2cd7c21685b4e8c763eec02961d4438b2ff7b13
Emmanuel Quattropani <emmanuel.quattropani@loteriacba.com.ar>
2019-01-18 12:37:45 -0300

    Estructura del controlador usuario

--- src/app/server/api/v1/controllers/usuarioController.js
+++ src/app/server/api/v1/controllers/usuarioController.js
@@ -1,4 +1,5 @@
 var oracle = require('../routes/conn');
+
 exports.GetAll = function(req,res){
   var db = oracle.conectarDB(res);
   var limite = req.params.limite;
@@ -11,4 +12,52 @@ exports.GetAll = function(req,res){
   return db.Usuario.findAll(filters).then(result => {
    return res.status(200).json(result);
   });
+}
+
+exports.GetBy = function(){
+
+}
+
+exports.GetById = function(){
+
+}
+
+exports.Save = function(){
+
+}
+
+exports.Update = function(){
+
+}
+
+exports.LockUserId() = function(){
+
+}
+
+exports.UnlockUserId() = function(){
+
+}
+
+exports.RestartPassword = function(){
+
+}
+
+exports.AddRole = function(){
+
+}
+
+exports.RemoveRole = function(){
+
+}
+
+exports.GetRolesAdd = function(){
+
+}
+
+exports.GetSystemasAdd = function(){
+
+}
+
+exports.GetSystemsMulti = function(){
+
 }
